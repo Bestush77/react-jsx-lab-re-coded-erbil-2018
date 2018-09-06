a805b970c5b02e0b600929a931d542c9c4802306
Bestun Gardi <bestun95@yahoo.com>
2018-09-06 07:01:37 +0000

    automatically backed up by learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -5,7 +5,11 @@ export default class FillerText extends React.Component {
   render(){
     return(
       <p>
+<<<<<<< HEAD
       I am a filler text. I can be used to fill your screen. Amazing!
+=======
+      I am a filler text. I can be used yo fill your screen. Amazing
+>>>>>>> b281aa7e592faef0d4177eeb8e296df3a35f49fc
       </p>
       )
   }
--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 
 import FillerText from './FillerText';
 
@@ -13,3 +14,20 @@ export default class Webpage extends React.Component {
     )
   }
 }
+=======
+import FillerText from './FillerText'
+
+export class Webpage extends React.Component {
+  render(){
+    return(
+    <div>
+    <h1>The world's coolest webpage</h1>
+      <FillerText />
+      <FillerText />
+    </div> 
+      
+      )
+  }
+    
+}
+>>>>>>> b281aa7e592faef0d4177eeb8e296df3a35f49fc
